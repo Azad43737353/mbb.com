9f6493b94a5a6548f0a141dceefcdd694f5584b1
AK.Azad <76525271+Azad43737353@users.noreply.github.com>
2022-12-30 23:51:09 +0600

    Add files via upload

---
+++ 11.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MBB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    
+  <img src="B-P (5).jpg" class="rounded float-start" alt="...">
+  <div class="text-center">
+  <img src="c.jpg" class="rounded" alt="...">
+</div>
+<img src="f.jpg" class="rounded float-end" alt="...">
+<div class="text-center">
+  <img src="d.jpg" class="rounded" alt="...">
+</div>
+<img src="B-P (5).jpg" class="rounded float-start" alt="...">
+
+<img src="f.jpg" class="rounded float-end" alt="...">
+<div class="text-center">
+  <img src="d.jpg" class="rounded" alt="...">
+</div>
+<img src="B-P (5).jpg" class="rounded float-start" alt="...">
+<div class="text-center">
+  <img src="g.webp" class="rounded" alt="...">
+</div>
+<img src="f.jpg" class="rounded float-end" alt="...">
+<img src="B-P (5).jpg" class="rounded float-start" alt="...">
+<img src="f.jpg" class="rounded float-end" alt="...">  <img src="B-P (5).jpg" class="rounded float-start" alt="...">
+<img src="f.jpg" class="rounded float-end" alt="...">
+<img src="B-P (5).jpg" class="rounded float-start" alt="...">
+<img src="f.jpg" class="rounded float-end" alt="...">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- slide end-->
+<?php
+    for($am=0; $am <=4; $am++){
+        echo('<img style="margin:10px; " src="2.jpeg"> <img style="margin:10px; " src="3.jpeg"> <img style="margin:10px; " src="4.png">');
+    };
+
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
